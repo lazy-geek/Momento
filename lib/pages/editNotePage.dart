d3f54121c78f06b811a5ddc41e0a229a592fe88e
abarar <abararmalek007@gmail.com>
2020-09-22 00:51:32 +0530

    created page to edit notes

---
+++ lib/pages/editNotePage.dart
@@ -0,0 +1,82 @@
+import 'package:flutter/material.dart';
+import 'package:notes/models/note.dart';
+import 'package:notes/view_model/note_list_view_model.dart';
+class EditNotePage extends StatefulWidget {
+  Note currentNote;
+  EditNotePage({this.currentNote});
+  @override
+  _EditNotePageState createState() => _EditNotePageState();
+}
+
+class _EditNotePageState extends State<EditNotePage> {
+  TextEditingController t1 = TextEditingController();
+  TextEditingController t2 = TextEditingController();
+  NoteListViewModel noteListViewModel;
+  bool isEdited=false;
+  @override
+  void initState() {
+    super.initState();
+    noteListViewModel= NoteListViewModel();
+    t1.text = widget.currentNote.title;
+    t2.text = widget.currentNote.content;
+  }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Add note'),
+        centerTitle: true,
+        leading: IconButton(
+            icon: Icon(Icons.arrow_back),
+            onPressed: () async {
+              if(isEdited){
+                Note newNote = Note.fromMap(
+                    {
+                    'title':'${t1.text}',
+                    'content':'${t2.text}',
+                    'isPinned':0,
+                    'date_created':'$DateTime.now().day',
+                    'last_updated':'$DateTime.now().day'
+                    },
+                  );
+                  newNote.id= widget.currentNote.id;
+                await noteListViewModel.updateNote(
+                  newNote
+                );
+              }
+              
+              Navigator.pop(context,isEdited);
+            }),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(5),
+        child: Column(
+          children: [
+            TextField(onChanged: (_){
+                isEdited = true;
+              },
+                autofocus: true,
+                style: TextStyle(fontSize: 25),
+                controller: t1,
+                maxLines: null,
+                decoration: InputDecoration(
+                    focusedBorder: InputBorder.none,
+                    hintText: 'Title',
+                    border: InputBorder.none),
+              ),
+            TextField(onChanged: (_){
+              isEdited = true;
+            },
+              maxLines: null,
+              controller: t2,
+              decoration: InputDecoration(
+                  focusedBorder: InputBorder.none,
+                  hintText: 'Type Something',
+                  border: InputBorder.none),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
