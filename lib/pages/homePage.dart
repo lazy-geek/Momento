c62a5faaf0338f74af427ef373696f07f7552eac
abarar <abararmalek007@gmail.com>
2020-09-22 00:52:35 +0530

    created the home page

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:notes/pages/addNotePage.dart';
+import 'package:notes/widgets/fab.dart';
+import 'package:notes/widgets/notes_list.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,10 +9,25 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  ScrollController s = ScrollController(keepScrollOffset: true);
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Notes'),
+        centerTitle: true,
+      ),
+      body: NotesList(s: s,),
+      floatingActionButton: Fab(
+        onPressed: () {
+          Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => AddNotePage()))
+              .then((value) => setState(() {}));
+        },
+        s: s,
+      ),
+     
     );
   }
 }
