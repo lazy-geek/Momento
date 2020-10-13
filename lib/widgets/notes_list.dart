af7d15e3b7f484220450b7cadb4afd2f082db503
abarar <abararmalek007@gmail.com>
2020-10-13 22:44:34 +0530

    extracted the card widget

--- lib/widgets/notes_list.dart
+++ lib/widgets/notes_list.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notes/models/note.dart';
-import 'package:notes/pages/editNotePage.dart';
 import 'package:notes/view_model/note_list_view_model.dart';
+import 'package:notes/widgets/note_card.dart';
 
 class NotesList extends StatefulWidget {
   ScrollController s;
@@ -20,41 +20,24 @@ class _NotesListState extends State<NotesList> {
 
   @override
   Widget build(BuildContext context) {
-    
     return FutureBuilder(
-        future: noteListViewModel.getAllNotes(),
-        builder: (context, AsyncSnapshot<List<Note>> snapshot) {
-    if (snapshot.connectionState == ConnectionState.none &&
-        snapshot.hasData == null) {
-      return Container();
-    }
-    if (snapshot.connectionState == ConnectionState.waiting) {
-      return Center(child: CircularProgressIndicator());
-    }
-    return ListView.builder(controller: widget.s,
-      itemCount: snapshot.data.length ?? 0,
-      itemBuilder: (context, index) {
-        return ListTile(
-          onTap: () async {
-              await Navigator.push(
-                  context,
-                  MaterialPageRoute(
-        builder: (context) => EditNotePage(
-              currentNote: snapshot.data[index],
-            ))).then((value) {
-                if (value) {
-                  setState(() {});
-                }
-              });
-              
-            },
-              key: ValueKey(snapshot.data[index].id),
-              title: Text("${snapshot.data[index].title}"),
-              subtitle: Text("${snapshot.data[index].id}"),
-            );
+      future: noteListViewModel.getAllNotes(),
+      builder: (context, AsyncSnapshot<List<Note>> snapshot) {
+        if (snapshot.connectionState == ConnectionState.none &&
+            snapshot.hasData == null) {
+          return Container();
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        }
+        return ListView.builder(physics: BouncingScrollPhysics(),
+          controller: widget.s,
+          itemCount: snapshot.data.length ?? 0,
+          itemBuilder: (context, index) {
+            return NoteCard(currentNote: snapshot.data[index]);
+          },
+        );
       },
     );
-        },
-      );
   }
 }
