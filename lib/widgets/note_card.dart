41ec0fc94d4c31503f0b2b074c5fea8febce944a
abarar <abararmalek007@gmail.com>
2021-02-21 15:59:41 +0530

    inkwell had issue so change it to gesture detector and fixed some margin and padding

--- lib/widgets/note_card.dart
+++ lib/widgets/note_card.dart
@@ -9,73 +9,81 @@ class NoteCard extends StatefulWidget {
   _NoteCardState createState() => _NoteCardState();
 }
 
+// padding: const EdgeInsets.fromLTRB(8.0, 2.0, 8.0, 2.0),
+// padding: const EdgeInsets.fromLTRB(3.0, 3.0, 3.0, 3.0),
+
 class _NoteCardState extends State<NoteCard> {
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.fromLTRB(8.0, 2.0, 8.0, 2.0),
-      child: InkWell(
-        onTap: () async {
-          await Navigator.push(
-              context,
-              MaterialPageRoute(
-                  builder: (context) => EditNotePage(
-                        currentNote: widget.currentNote,
-                      ))).then((value) {
-            if (value == true) {
-              setState(() {});
-            }
-          });
-        },
-        child: Container(
-          padding: EdgeInsets.all(15.0),
-          decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(10.0),
-              border:
-                  Border.all(width: 1.0, color: Colors.grey.withOpacity(0.2))),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              widget.currentNote.title.isNotEmpty
-                  ? Padding(
-                      // padding:const EdgeInsets.fromLTRB(8.0, 2.0, 2.0, 7.0),
-                      padding: EdgeInsets.all(0.0),
-                      child: Text(
-                        "${widget.currentNote.title}",
-                        overflow:TextOverflow.ellipsis,
-                        maxLines: 1,
-                        style: TextStyle(
+    // TODO : impement inkwell instead of GestureDetector
+    return GestureDetector(
+      onTap: () async {
+        await Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => EditNotePage(
+                      currentNote: widget.currentNote,
+                    ))).then((value) {
+          if (value == true) {
+            setState(() {});
+          }
+        });
+      },
+      child: Container(
+        // use [margin] property only if using list layout , don't use it with grid layout
+        margin: EdgeInsets.symmetric(vertical: 5.0),
+        padding: EdgeInsets.all(15.0),
+        decoration: BoxDecoration(
+            // color: Colors.white,
+            color: Color(0xFF2c3342),
+            borderRadius: BorderRadius.circular(10.0),
+            border:
+                Border.all(width: 1.0, color: Colors.grey.withOpacity(0.2))),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            widget.currentNote.title.isNotEmpty
+                ? Padding(
+                    // padding:const EdgeInsets.fromLTRB(8.0, 2.0, 2.0, 7.0),
+                    padding: EdgeInsets.all(0.0),
+                    child: Text(
+                      "${widget.currentNote.title}",
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 1,
+                      style: TextStyle(
                           fontWeight: FontWeight.w500,
                           fontSize: 18.0,
-                        ),
-                      ),
-                    )
-                  : Container(
-                      height: 0.0,
-                      width: 0.0,
+                          color: Colors.white),
                     ),
-              SizedBox(
-                height: 5.0,
-              ),
-              Padding(
-                // padding: const EdgeInsets.fromLTRB(8.0, 0.0, 2.0, 8.0),
-                padding: EdgeInsets.all(0.0),
-                child: Text(
-                  "${widget.currentNote.content}",
-                  overflow: TextOverflow.ellipsis,
-                  maxLines: 10,
-                  style: TextStyle(
-                      // height: 1.3,
-                      fontSize: 16.0,
-                      color: Colors.grey.shade600
-                      // color: Colors.black54,
-                      ),
+                  )
+                : Container(
+                    height: 0.0,
+                    width: 0.0,
+                  ),
+            SizedBox(
+              height: 5.0,
+            ),
+            Padding(
+              // padding: const EdgeInsets.fromLTRB(8.0, 0.0, 2.0, 8.0),
+              padding: EdgeInsets.all(0.0),
+              child: Text(
+                "${widget.currentNote.content}",
+                overflow: TextOverflow.ellipsis,
+                maxLines: 10,
+                style: TextStyle(
+                  // height: 1.3,
+                  fontSize: 16.0,
+                  // light mode
+                  // color: Colors.grey.shade600
+
+                  //dark mode
+                  color: Colors.white.withOpacity(0.8),
+                  // color: Colors.black54,
                 ),
               ),
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
