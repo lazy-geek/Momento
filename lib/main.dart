f4660dcf25cf6ecbefe3a3f527777c95bb987b92
abarar <abararmalek007@gmail.com>
2021-02-21 16:06:27 +0530

    fomatting

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:notes/pages/homePage.dart';
 
-
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
