7fa535fabfad3dfef48cf489ffc12aa9d9a056ff
abarar <abararmalek007@gmail.com>
2021-02-24 18:24:55 +0530

    removed scrollconroller and logic for fade effect on fab

--- lib/widgets/fab.dart
+++ lib/widgets/fab.dart
@@ -3,51 +3,27 @@ import 'package:flutter/rendering.dart';
 
 class Fab extends StatefulWidget {
   VoidCallback onPressed;
-  ScrollController s;
-  Fab({this.onPressed,this.s});
+  Fab({this.onPressed});
   @override
   _FabState createState() => _FabState();
 }
 
 class _FabState extends State<Fab> {
-  bool hidefab = false;
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    widget.s.addListener(() {
-        if(widget.s.position.userScrollDirection ==ScrollDirection.forward)
-      {
-        
-        if(hidefab == false){
-          setState(() {
-            
-          hidefab= true;
-          });
-        }
-      }else if(widget.s.position.userScrollDirection ==ScrollDirection.reverse)
-      {
-        if(hidefab == true){
-          setState(() {
-            
-          hidefab= false;
-          });
-        }
-      }
-      
-      
-    });
-  }
   @override
   Widget build(BuildContext context) {
-    return AnimatedOpacity(duration: Duration(milliseconds: 500),opacity: hidefab? 0 :1,
-          child: IgnorePointer(ignoring: hidefab,
-                      child: FloatingActionButton(
-            onPressed: widget.onPressed,
-            tooltip: 'Add Note',
-            child: Icon(Icons.add),
-        ),
-          ),
+    // return AnimatedOpacity(duration: Duration(milliseconds: 500),opacity: hidefab? 0 :1,
+    //       child: IgnorePointer(ignoring: hidefab,
+    //                   child: FloatingActionButton(
+    //         onPressed: widget.onPressed,
+    //         tooltip: 'Add Note',
+    //         child: Icon(Icons.add),
+    //     ),
+    //       ),
+    // );
+    return FloatingActionButton(
+      onPressed: widget.onPressed,
+      tooltip: 'Add Note',
+      child: Icon(Icons.add),
     );
   }
 }
