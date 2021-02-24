97a95f9fb2a654f06c49c5bd2553f187dd090de1
abarar <abararmalek007@gmail.com>
2021-02-24 18:26:12 +0530

    removed the old way of updatinf ui and instead used consumer and providers from riverpod package

--- lib/widgets/notes_list.dart
+++ lib/widgets/notes_list.dart
@@ -1,58 +1,41 @@
 import 'package:flutter/material.dart';
-import 'package:notes/models/note.dart';
-import 'package:notes/view_model/note_list_view_model.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:notes/widgets/note_card.dart';
+import 'package:notes/providers/providers.dart';
 
 class NotesList extends StatefulWidget {
-  ScrollController s;
-  NotesList({this.s});
   @override
   _NotesListState createState() => _NotesListState();
 }
 
 class _NotesListState extends State<NotesList> {
-  NoteListViewModel noteListViewModel;
-  @override
-  void initState() {
-    super.initState();
-    noteListViewModel = NoteListViewModel();
-  }
-
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: noteListViewModel.getAllNotes(),
-      builder: (context, AsyncSnapshot<List<Note>> snapshot) {
-        if (snapshot.connectionState == ConnectionState.none &&
-            snapshot.hasData == null) {
-          return Container();
-        }
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(child: CircularProgressIndicator());
-        }
-        return CustomScrollView(
-          slivers: [
-            // This [Padding] affects the area between the edge of the screen and the [ListView]
-            SliverPadding(
-              padding: EdgeInsets.fromLTRB(8.0, 30.0, 8.0, 8.0),
-              sliver: SliverList(
-                delegate: SliverChildBuilderDelegate(
-                  (contex, index) {
-                    return NoteCard(currentNote: snapshot.data[index]);
-                  },
-                  childCount: snapshot.data.length ?? 0,
+    return Consumer(
+      builder: (context, watch, child) {
+        AsyncValue asyncnotelist = watch(AllNotesProvider);
+        return asyncnotelist.when(
+          data: (data) {
+            return CustomScrollView(
+              slivers: [
+                // This [Padding] affects the area between the edge of the screen and the [ListView]
+                SliverPadding(
+                  padding: EdgeInsets.fromLTRB(8.0, 30.0, 8.0, 8.0),
+                  sliver: SliverList(
+                    delegate: SliverChildBuilderDelegate(
+                      (contex, index) {
+                        return NoteCard(currentNote: data[index]);
+                      },
+                      childCount: data.length ?? 0,
+                    ),
+                  ),
                 ),
-              ),
-            ),
-          ],
+              ],
+            );
+          },
+          loading: () => Center(child: CircularProgressIndicator()),
+          error: (errr, stack) => Text('error'),
         );
-        // return ListView.builder(physics: BouncingScrollPhysics(),
-        //   controller: widget.s,
-        //   itemCount: snapshot.data.length ?? 0,
-        //   itemBuilder: (context, index) {
-        //     return NoteCard(currentNote: snapshot.data[index]);
-        //   },
-        // );
       },
     );
   }
