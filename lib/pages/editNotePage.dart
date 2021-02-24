6737e6c8cb98c00afc549135d3806873a9cea04d
abarar <abararmalek007@gmail.com>
2021-02-24 18:05:36 +0530

    removed the old logic to update the ui on state change and added consumer

--- lib/pages/editNotePage.dart
+++ lib/pages/editNotePage.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:notes/models/note.dart';
-import 'package:notes/view_model/note_list_view_model.dart';
+import 'package:notes/providers/providers.dart';
 
 class EditNotePage extends StatefulWidget {
   Note currentNote;
@@ -12,12 +13,10 @@ class EditNotePage extends StatefulWidget {
 class _EditNotePageState extends State<EditNotePage> {
   TextEditingController t1 = TextEditingController();
   TextEditingController t2 = TextEditingController();
-  NoteListViewModel noteListViewModel;
   bool isEdited = false;
   @override
   void initState() {
     super.initState();
-    noteListViewModel = NoteListViewModel();
     t1.text = widget.currentNote.title;
     t2.text = widget.currentNote.content;
   }
@@ -32,62 +31,70 @@ class _EditNotePageState extends State<EditNotePage> {
         centerTitle: true,
         // Dark Mode color
         backgroundColor: Color(0xFF2c3342),
-        leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            onPressed: () async {
-              if (isEdited) {
-                Note newNote = Note.fromMap(
-                  {
-                    'title': '${t1.text}',
-                    'content': '${t2.text}',
-                    'isPinned': 0,
-                    'date_created': '$DateTime.now().day',
-                    'last_updated': '$DateTime.now().day'
-                  },
-                );
-                newNote.id = widget.currentNote.id;
-                await noteListViewModel.updateNote(newNote);
-              }
-              Navigator.pop(context, isEdited);
-            }),
+        leading: Consumer(
+          builder: (context, watch, child) {
+            return IconButton(
+                icon: Icon(Icons.arrow_back),
+                onPressed: () async {
+                  if (isEdited) {
+                    Note newNote = Note.fromMap(
+                      {
+                        'title': '${t1.text}',
+                        'content': '${t2.text}',
+                        'isPinned': 0,
+                        'date_created': '$DateTime.now().day',
+                        'last_updated': '$DateTime.now().day'
+                      },
+                    );
+                    newNote.id = widget.currentNote.id;
+                    // print(widget.currentNote.content);
+                    await context
+                        .read(NoteListViewModelProvider)
+                        .updateNote(newNote);
+                  }
+                  Navigator.pop(context);
+                });
+          },
+        ),
       ),
       body: SingleChildScrollView(
         child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 15.0),
-        child: Column(
-          children: [
-            SizedBox(
-              height: 26.0,
-            ),
-            TextField(
-              onChanged: (_) {
-                isEdited = true;
-              },
-              showCursor: true,
-              autofocus: true,
-              style: TextStyle(fontSize: 25, color: Colors.white),
-              controller: t1,
-              maxLines: null,
-              decoration: InputDecoration(
-                  hintStyle: TextStyle(color: Colors.grey.shade400),
-                  focusedBorder: InputBorder.none,
-                  hintText: 'Title',
-                  border: InputBorder.none),
-            ),
-            TextField(
-              onChanged: (_) {
-                isEdited = true;
-              },
-              style: TextStyle(color: Colors.white),
-              maxLines: null,
-              controller: t2,
-              decoration: InputDecoration(
-                  focusedBorder: InputBorder.none,
-                  hintText: 'Type Something',
-                  hintStyle: TextStyle(color: Colors.grey.shade400),
-                  border: InputBorder.none),
-            )
-          ],
+          padding: EdgeInsets.symmetric(horizontal: 15.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 26.0,
+              ),
+              TextField(
+                onChanged: (_) {
+                  isEdited = true;
+                },
+                showCursor: true,
+                autofocus: true,
+                style: TextStyle(fontSize: 25, color: Colors.white),
+                controller: t1,
+                maxLines: null,
+                decoration: InputDecoration(
+                    hintStyle: TextStyle(color: Colors.grey.shade400),
+                    focusedBorder: InputBorder.none,
+                    hintText: 'Title',
+                    border: InputBorder.none),
+              ),
+              TextField(
+                onChanged: (_) {
+                  isEdited = true;
+                },
+                style: TextStyle(color: Colors.white),
+                maxLines: null,
+                controller: t2,
+                decoration: InputDecoration(
+                    focusedBorder: InputBorder.none,
+                    hintText: 'Type Something',
+                    hintStyle: TextStyle(color: Colors.grey.shade400),
+                    border: InputBorder.none),
+              )
+            ],
+          ),
         ),
       ),
     );
