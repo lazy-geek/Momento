83e34231954b62b647c4ea3e3d097116fe2bede4
abarar <abararmalek007@gmail.com>
2021-02-24 18:13:36 +0530

    added changenotifier to the NoteListViewModel Class and got getAllNotes and updateNote functions to work with it

--- lib/view_model/note_list_view_model.dart
+++ lib/view_model/note_list_view_model.dart
@@ -1,37 +1,46 @@
+import 'package:flutter/foundation.dart';
 import 'package:notes/models/note.dart';
 import 'package:notes/utils/databaseHelper.dart';
-class NoteListViewModel{
-List<Note> notes_list = [];
-bool isLoading = false;
 
-Future<int> addNote(Note note) async {
+class NoteListViewModel extends ChangeNotifier {
+  List<Note> notes_list;
+  NoteListViewModel() {
+    this.notes_list = [];
+  }
+
+  Future<int> addNote(Note note) async {
     var result = await DatabaseHelper.instance.addNote(note);
-    await getAllNotes();
+    // await getAllNotes();
+    notifyListeners();
     return result;
   }
 
-Future<List<Note>> getAllNotes() async {
-  notes_list = await DatabaseHelper.instance.getAllNotes();
-  return notes_list ?? []; //  if(notes_list == null) return [];
+  Future<void> getAllNotes() async {
+    notes_list = await DatabaseHelper.instance.getAllNotes();
+    print(notes_list.last.content);
+
+    notifyListeners();//  if(notes_list == null) return [];
   }
 
-Future<Note> getNote(int noteId) async {
+  Future<Note> getNote(int noteId) async {
     return notes_list.map((element) {
-      if(element.id == noteId){
+      if (element.id == noteId) {
         return element;
       }
-      }).first;
-}
+    }).first;
+  }
 
-Future<int> deleteNote(int noteid) async {
+  Future<int> deleteNote(int noteid) async {
     var result = await DatabaseHelper.instance.deleteNote(noteid);
-    await getAllNotes();
+    // await getAllNotes();
+    notifyListeners();
     return result;
   }
 
-Future<int> updateNote(Note note) async {
+  Future<int> updateNote(Note note) async {
     var result = await DatabaseHelper.instance.updateNote(note);
-    await getAllNotes();
+    notes_list = await DatabaseHelper.instance.getAllNotes();
+    notifyListeners();
     return result;
   }
 }
