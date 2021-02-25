f4467c3b7f20197b471b1cc21f2ea6521d046e1b
abarar <abararmalek007@gmail.com>
2021-02-25 12:36:02 +0530

    added scrollconroller using a provider  and added logic to animate the Fab on Scroll

--- lib/widgets/fab.dart
+++ lib/widgets/fab.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:notes/providers/providers.dart';
 
 class Fab extends StatefulWidget {
   VoidCallback onPressed;
@@ -8,22 +10,63 @@ class Fab extends StatefulWidget {
   _FabState createState() => _FabState();
 }
 
-class _FabState extends State<Fab> {
+class _FabState extends State<Fab> with SingleTickerProviderStateMixin {
+  AnimationController _hideFabAnimController;
+  ScrollController _scrollController;
+  @override
+  void initState() {
+    super.initState();
+    _hideFabAnimController = AnimationController(
+      vsync: this,
+      duration: Duration(seconds: 3),
+      value: 1,
+    );
+  }
+
+  @override
+  void dispose() {
+    _hideFabAnimController.dispose();
+    _scrollController.dispose();
+    super.dispose();
+  }
+  // the setupScroll(BuildContext context) function is used to addListener to the scrollController and animate the Fab based on
+  // the ScrollDirection, it takes [BuildContext] as the input parameter to access the provider using the
+  // [BuildContext] provided by the build method.
+  void setupScroll(BuildContext context) {
+    _scrollController = context.read(ScrollControllerProvider);
+    _scrollController.addListener(() {
+      switch (_scrollController.position.userScrollDirection) {
+        case ScrollDirection.forward:
+          _hideFabAnimController.forward();
+          break;
+
+        case ScrollDirection.reverse:
+          _hideFabAnimController.reverse();
+          break;
+        case ScrollDirection.idle:
+          break;
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    // return AnimatedOpacity(duration: Duration(milliseconds: 500),opacity: hidefab? 0 :1,
-    //       child: IgnorePointer(ignoring: hidefab,
-    //                   child: FloatingActionButton(
-    //         onPressed: widget.onPressed,
-    //         tooltip: 'Add Note',
-    //         child: Icon(Icons.add),
-    //     ),
-    //       ),
-    // );
-    return FloatingActionButton(
-      onPressed: widget.onPressed,
-      tooltip: 'Add Note',
-      child: Icon(Icons.add),
+    // we call setupScroll(BuildContext context) function with the [BuildContext] , so we can access provider
+    // using the [BuildContext].
+    setupScroll(context);
+    return FadeTransition(
+      opacity: _hideFabAnimController,
+      child: ScaleTransition(
+        scale: _hideFabAnimController,
+        child: IgnorePointer(
+          ignoring: _hideFabAnimController.value == 1 ? true : false,
+          child: FloatingActionButton(
+            onPressed: widget.onPressed,
+            tooltip: 'Add Note',
+            child: Icon(Icons.add),
+          ),
+        ),
+      ),
     );
   }
 }
