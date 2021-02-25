19b479b52b76bc64c80ae25444f937e3ca784987
abarar <abararmalek007@gmail.com>
2021-02-25 17:56:22 +0530

    made Note class a ChangeNotifier and added update method
    Note class is now a ChangeNotifier because it need to Notify it's listeners when the Note has changed

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -1,13 +1,23 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:notes/utils/databaseHelper.dart';
 
-class Note {
+class Note extends ChangeNotifier {
   int id;
   String title;
   String content;
   int isPinned;
   String date_created;
   String last_updated;
-  Note({this.id,this.title,this.content,this.isPinned,this.date_created,this.last_updated});
 
+  Note({
+    this.id,
+    this.title,
+    this.content,
+    this.isPinned,
+    this.date_created,
+    this.last_updated,
+  });
 
   factory Note.fromMap(Map<String, dynamic> map) {
     return new Note(
@@ -19,15 +29,28 @@ class Note {
         last_updated: map['last_updated']);
   }
 
-  Map<String, dynamic> toMap(){
+  Map<String, dynamic> toMap() {
     Map<String, dynamic> map = {
-      'title':title,
-      'content':content,
-      'isPinned':isPinned,
-      'date_created':date_created,
-      'last_updated':last_updated
+      'title': title,
+      'content': content,
+      'isPinned': isPinned,
+      'date_created': date_created,
+      'last_updated': last_updated
     };
 
     return map;
+  }
+
+  Future<int> update(Note note) async {
+    var result = await DatabaseHelper.instance.updateNote(note);
+    this.content = note.content;
+    this.date_created = note.date_created;
+    this.id = note.id;
+    this.isPinned = note.isPinned;
+    this.last_updated = note.last_updated;
+    this.last_updated = note.last_updated;
+    this.title = note.title;
+    notifyListeners();
+    return result;
   }
 }
