1241e19c9e19ee5f9f1c7c35a498a486f85385c0
abarar <abararmalek007@gmail.com>
2021-02-25 14:23:00 +0530

    added SingleChildScrollView to fix overflow issue

--- lib/pages/addNotePage.dart
+++ lib/pages/addNotePage.dart
@@ -47,32 +47,38 @@ class _AddNotePageState extends State<AddNotePage> {
                 });
           },
         ),
-      body: Container(padding: EdgeInsets.symmetric(horizontal: 15.0),
-        child: Column(
-          children: [
-            SizedBox(height: 26.0,),
-            TextField(
-              autofocus: true,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          padding: EdgeInsets.symmetric(horizontal: 15.0),
+          child: Column(
+            children: [
+              SizedBox(
+                height: 26.0,
+              ),
+              TextField(
+                autofocus: true,
                 style: TextStyle(fontSize: 25, color: Colors.white),
-              controller: t1,
-              maxLines: null,
-              decoration: InputDecoration(
-                  focusedBorder: InputBorder.none,
-                  hintText: 'Title',
+                controller: t1,
+                maxLines: null,
+                decoration: InputDecoration(
+                    focusedBorder: InputBorder.none,
+                    hintText: 'Title',
                     hintStyle: TextStyle(color: Colors.grey.shade400),
-                  border: InputBorder.none),
-            ),
-            TextField(
-              maxLines: null,
-              controller: t2,
+                    border: InputBorder.none),
+              ),
+              TextField(
+                maxLines: null,
+                controller: t2,
                 style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                  focusedBorder: InputBorder.none,
-                  hintText: 'Type Something',
+                decoration: InputDecoration(
+                    focusedBorder: InputBorder.none,
+                    hintText: 'Type Something',
                     hintStyle: TextStyle(color: Colors.grey.shade400),
-                  border: InputBorder.none),
-            )
-          ],
+                    border: InputBorder.none),
+              )
+            ],
+          ),
         ),
       ),
     );
