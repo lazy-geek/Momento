48ad103078329fb544c5cb3b38704b274f65f12b
abarar <abararmalek007@gmail.com>
2021-02-26 13:50:39 +0530

    changed location of consumer
    moved consumer to be parent whole widget because the provider was needed in determining whether to show apply or not

--- lib/widgets/note_card.dart
+++ lib/widgets/note_card.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:notes/models/note.dart';
 import 'package:notes/pages/editNotePage.dart';
 import 'package:notes/providers/providers.dart';
+import 'package:notes/view_model/note_list_view_model.dart';
 
 class NoteCard extends StatefulWidget {
   int index;
@@ -19,40 +20,47 @@ class _NoteCardState extends State<NoteCard> {
   Widget build(BuildContext context) {
     // TODO : impement inkwell instead of GestureDetector
 
-    return GestureDetector(
-      onTap: () async {
-        await Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => EditNotePage(
-              index: widget.index,
-            ),
-          ),
-        );
-      },
-      child: Container(
-        // use [margin] property only if using list layout , don't use it with grid layout
-        margin: EdgeInsets.symmetric(vertical: 5.0),
-        padding: EdgeInsets.all(15.0),
-        decoration: BoxDecoration(
+    // we use [Consumer] here in order to minimize the rebuild by only rebuilding this
+    // instance [NoteCard] when this Note changes, using the [NoteProvider] as oppose to
+    // rebuilding the whole listview or gridview which happens in the case where
+    // the current Note was provided from [AllNotesProvider] which causes the whole list rebuild
+    // whenever a single Note changes because [AllNotesProvider] provies the note list, not a single note.
+    return Consumer(
+      builder: (context, watch, child) {
+        print('inside note card at index ${widget.index}');
+        Note note = watch(NoteProvider(widget.index));
+        final viewModel = watch(NoteListViewModelProvider);
+        ViewType v = viewModel.v;
+        return GestureDetector(
+          onTap: () async {
+            await Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => EditNotePage(
+                  index: widget.index,
+                ),
+              ),
+            );
+          },
+          child: Container(
+            // use [margin] property only if using list layout , don't use it with grid layout
+            margin: v== ViewType.List? EdgeInsets.symmetric(vertical: 5.0) : EdgeInsets.symmetric(vertical:0.0),
+            padding: EdgeInsets.all(15.0),
+            decoration: BoxDecoration(
+              //   boxShadow: [
+              //   BoxShadow(color: Color(0xFF2c3342).withOpacity(0.8),blurRadius: 1.0,offset: Offset(-4.0,4.0)),
+              //   BoxShadow(color: Color(0xFF2c3342).withOpacity(0.8),blurRadius: 1.0,offset: Offset(4.0,-4.0))
+              // ],
               // color: Colors.white,
               color: Color(0xFF354252),
-          borderRadius: BorderRadius.circular(10.0),
-          border: Border.all(
-            width: 1.0,
-            color: Colors.grey.withOpacity(0.2),
-          ),
-        ),
-        // we use [Consumer] here in order to minimize the rebuild by only rebuilding this
-        // instance [NoteCard] when this Note changes, using the [NoteProvider] as oppose to 
-        // rebuilding the whole listview or gridview which happens in the case where
-        // the current Note was provided from [AllNotesProvider] which causes the whole list rebuild
-        // whenever a single Note changes because [AllNotesProvider] provies the note list, not a single note.
-        child: Consumer(
-          builder: (context, watch, child) {
-            print('inside note card at index ${widget.index}');
-            Note note = watch(NoteProvider(widget.index));
-            return Column(
+              borderRadius: BorderRadius.circular(10.0),
+              // border: Border.all(
+              //   width: 1.0,
+              //   color: Colors.grey.withOpacity(0.2),
+              // ),
+            ),
+
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
@@ -89,7 +97,6 @@ class _NoteCardState extends State<NoteCard> {
                       fontSize: 16.0,
                       // light mode
                       // color: Colors.grey.shade600
-
                       //dark mode
                       color: Colors.white.withOpacity(0.8),
                       // color: Colors.black54,
@@ -97,10 +104,10 @@ class _NoteCardState extends State<NoteCard> {
                   ),
                 ),
               ],
-            );
-          },
-        ),
-      ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
