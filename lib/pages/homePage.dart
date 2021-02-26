c84f244ca96b401fb773292d2d06dd665b9b4ec8
abarar <abararmalek007@gmail.com>
2021-02-26 13:59:17 +0530

    added way to change layout type
    added a toggle button to switch between grid and list layout

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:notes/pages/addNotePage.dart';
+import 'package:notes/providers/providers.dart';
+import 'package:notes/view_model/note_list_view_model.dart';
 import 'package:notes/widgets/fab.dart';
 import 'package:notes/widgets/notes_grid.dart';
 import 'package:notes/widgets/notes_list.dart';
@@ -16,13 +19,41 @@ class _HomePageState extends State<HomePage> {
       appBar: AppBar(
         title: Text('Notes'),
         centerTitle: true,
+        actions: [
+          Consumer(
+            builder: (context, watch, child) {
+              final viewModel = watch(NoteListViewModelProvider);
+              LayoutType layout = viewModel.layout;
+              if (layout == LayoutType.Grid) {
+                return IconButton(
+                  icon: Icon(Icons.list),
+                  onPressed: () {
+                    viewModel.toggleView();
+                  },
+                );
+              } else {
+                return IconButton(
+                  icon: Icon(Icons.grid_view),
+                  onPressed: () {
+                    viewModel.toggleView();
+                  },
+                );
+              }
+            },
+          ),
+        ],
         // Dark Mode color
         backgroundColor: Color(0xFF293440),
       ),
       // Dark Mode color
       backgroundColor: Color(0xFF293440),
       // body: NotesList(),
-      body: NotesGrid(),
+      body: Consumer(
+        builder: (context, watch, child) {
+          LayoutType layout = watch(NoteListViewModelProvider).layout;
+          return layout == LayoutType.Grid ? NotesGrid() : NotesList();
+        },
+      ),
       floatingActionButton: Fab(
         onPressed: () {
           Navigator.push(
