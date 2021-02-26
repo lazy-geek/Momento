a9c13a137471d7b1d7bd64ec5baf1f4a5cd0c697
abarar <abararmalek007@gmail.com>
2021-02-26 14:44:41 +0530

    added multiselect
    added variable to store if the note is selected or not and added function to toggle it

--- lib/models/note.dart
+++ lib/models/note.dart
@@ -9,6 +9,7 @@ class Note extends ChangeNotifier {
   int isPinned;
   String date_created;
   String last_updated;
+  bool isSelected = false;
 
   Note({
     this.id,
@@ -52,5 +53,10 @@ class Note extends ChangeNotifier {
     this.title = note.title;
     notifyListeners();
     return result;
+  }
+
+  void toggleSelection(){
+    isSelected = !isSelected;
+    notifyListeners();
   }
 }
