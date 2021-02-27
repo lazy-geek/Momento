ba824a1654150c080239ef3947341e9316122e22
abarar <abararmalek007@gmail.com>
2021-02-27 14:15:47 +0530

    temp fix : spash effect flowing out of card when using inkwell
    temporary fixed an issue were spash effect was flowing out of card when using inkwell by replacing it with InkResponce Widget and setting it's containedInkWell property to false, also enabled haptic feed back on long press using inkResponse's enableFeedback property

--- lib/widgets/note_card.dart
+++ lib/widgets/note_card.dart
@@ -18,7 +18,6 @@ class NoteCard extends StatefulWidget {
 class _NoteCardState extends State<NoteCard> {
   @override
   Widget build(BuildContext context) {
-    // TODO : impement inkwell instead of GestureDetector
 
     // we use [Consumer] here in order to minimize the rebuild by only rebuilding this
     // instance [NoteCard] when this Note changes, using the [NoteProvider] as oppose to
@@ -33,109 +32,118 @@ class _NoteCardState extends State<NoteCard> {
         LayoutType layout = viewModel.layout;
 
         final selectednotes = watch(SelectedNotesProvider);
-         // [isSelected] will be true if current note is selected
+        // [isSelected] will be true if current note is selected
         bool isSelected = selectednotes.notes_list.contains(note);
-        return GestureDetector(
-          onTap: () async {
-            // check if current or any of the notes in the list is selected
-            // if current note is selected and user presses on it , then deselect it
-            // by removing it from the selectednotes list
-            if (isSelected) {
-              selectednotes.remove(note);
-            } else if (selectednotes.notes_list.isNotEmpty) {
-              // if current note is not selected but any of the other note is selected
-              // then select the current note when user presses on it by adding it to the selectednotes list
-              selectednotes.add(note);
-            } else {
-              await Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => EditNotePage(
-                    index: widget.index,
-                  ),
-                ),
-              );
-            }
-          },
-          onLongPress: () {
-            // select the note on long press
-            selectednotes.add(note);
-          },
-          child: Container(
-            // the reason i used [foregroundDecoration] instead of simple [Decoration] property is that
-            // the [foregroundDecoration] property draws the decoration on top of the Container so
-            // it doesn't interfear with spacing and layout of the nearby [NoteCard] widgets in the grid or list
-            // if we used simple [Decoration] properrty then when ever we select the widget and the Container's
-            // border are drawn then the surrounding widgets get pushed by some pixels
-            foregroundDecoration: BoxDecoration(
-              color: isSelected ? Colors.white.withOpacity(0.08):null,
-              border: isSelected
-                  ? Border.all(
-                      width: 1.8,
-                      color: Colors.white,
-                    )
-                  : Border.all(width: 0.0),
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            // use [margin] property only if using list layout , don't use it with grid layout
-            margin: layout == LayoutType.List
-                ? EdgeInsets.symmetric(vertical: 5.0)
-                : EdgeInsets.symmetric(vertical: 0.0),
-            padding: EdgeInsets.all(15.0),
-            decoration: BoxDecoration(
-              //   boxShadow: [
-              //   BoxShadow(color: Color(0xFF2c3342).withOpacity(0.8),blurRadius: 1.0,offset: Offset(-4.0,4.0)),
-              //   BoxShadow(color: Color(0xFF2c3342).withOpacity(0.8),blurRadius: 1.0,offset: Offset(4.0,-4.0))
-              // ],
-              // color: Colors.white,
-              color: Color(0xFF354252),
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                note.title.isNotEmpty
-                    ? Padding(
-                        // padding:const EdgeInsets.fromLTRB(8.0, 2.0, 2.0, 7.0),
-                        padding: EdgeInsets.all(0.0),
-                        child: Text(
-                          "${note.title}",
-                          overflow: TextOverflow.ellipsis,
-                          maxLines: 1,
-                          style: TextStyle(
-                              fontWeight: FontWeight.w500,
-                              fontSize: 18.0,
-                              color: Colors.white),
-                        ),
-                      )
-                    : Container(
-                        height: 0.0,
-                        width: 0.0,
-                      ),
-                SizedBox(
-                  height: 5.0,
-                ),
-                Padding(
-                  // padding: const EdgeInsets.fromLTRB(8.0, 0.0, 2.0, 8.0),
-                  padding: EdgeInsets.all(0.0),
-                  child: Text(
-                    "${note.content}",
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 10,
-                    style: TextStyle(
-                      // height: 1.3,
-                      fontSize: 16.0,
-                      // light mode
-                      // color: Colors.grey.shade600
-                      //dark mode
-                      color: Colors.white.withOpacity(0.8),
-                      // color: Colors.black54,
+        return Material(
+          type: MaterialType.transparency,
+          borderRadius: BorderRadius.circular(10.0),
+          child: InkResponse(
+            borderRadius: BorderRadius.circular(10.0),
+            // set [containedInkWell] to false to fix spash effect leaking outside the card
+            containedInkWell: false,
+            // set [enableFeedback] true to enable vibration on long press
+            enableFeedback: true,
+            onTap: () async {
+              // check if current or any of the notes in the list is selected
+              // if current note is selected and user presses on it , then deselect it
+              // by removing it from the selectednotes list
+              if (isSelected) {
+                selectednotes.remove(note);
+              } else if (selectednotes.notes_list.isNotEmpty) {
+                // if current note is not selected but any of the other note is selected
+                // then select the current note when user presses on it by adding it to the selectednotes list
+                selectednotes.add(note);
+              } else {
+                await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => EditNotePage(
+                      index: widget.index,
                     ),
                   ),
-                ),
-              ],
+                );
+              }
+            },
+            onLongPress: () {
+              // select the note on long press
+              selectednotes.add(note);
+            },
+            child: Container(
+              // the reason i used [foregroundDecoration] instead of simple [Decoration] property is that
+              // the [foregroundDecoration] property draws the decoration on top of the Container so
+              // it doesn't interfear with spacing and layout of the nearby [NoteCard] widgets in the grid or list
+              // if we used simple [Decoration] properrty then when ever we select the widget and the Container's
+              // border are drawn then the surrounding widgets get pushed by some pixels
+              foregroundDecoration: BoxDecoration(
+                color: isSelected ? Colors.white.withOpacity(0.08) : null,
+                border: isSelected
+                    ? Border.all(
+                        width: 1.8,
+                        color: Colors.white,
+                      )
+                    : Border.all(width: 0.0),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              // use [margin] property only if using list layout , don't use it with grid layout
+              margin: layout == LayoutType.List
+                  ? EdgeInsets.symmetric(vertical: 5.0)
+                  : EdgeInsets.symmetric(vertical: 0.0),
+              padding: EdgeInsets.all(15.0),
+              decoration: BoxDecoration(
+                //   boxShadow: [
+                //   BoxShadow(color: Color(0xFF2c3342).withOpacity(0.8),blurRadius: 1.0,offset: Offset(-4.0,4.0)),
+                //   BoxShadow(color: Color(0xFF2c3342).withOpacity(0.8),blurRadius: 1.0,offset: Offset(4.0,-4.0))
+                // ],
+                // color: Colors.white,
+                color: Color(0xFF354252),
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  note.title.isNotEmpty
+                      ? Padding(
+                          // padding:const EdgeInsets.fromLTRB(8.0, 2.0, 2.0, 7.0),
+                          padding: EdgeInsets.all(0.0),
+                          child: Text(
+                            "${note.title}",
+                            overflow: TextOverflow.ellipsis,
+                            maxLines: 1,
+                            style: TextStyle(
+                                fontWeight: FontWeight.w500,
+                                fontSize: 18.0,
+                                color: Colors.white),
+                          ),
+                        )
+                      : Container(
+                          height: 0.0,
+                          width: 0.0,
+                        ),
+                  SizedBox(
+                    height: 5.0,
+                  ),
+                  Padding(
+                    // padding: const EdgeInsets.fromLTRB(8.0, 0.0, 2.0, 8.0),
+                    padding: EdgeInsets.all(0.0),
+                    child: Text(
+                      "${note.content}",
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 10,
+                      style: TextStyle(
+                        // height: 1.3,
+                        fontSize: 16.0,
+                        // light mode
+                        // color: Colors.grey.shade600
+                        //dark mode
+                        color: Colors.white.withOpacity(0.8),
+                        // color: Colors.black54,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         );
