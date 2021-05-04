62cff7facff8fbe7c5f0b37fc205fe27251d8567
abarar <abararmalek007@gmail.com>
2021-05-04 13:18:11 +0530

    added google notes like searchbar/appbar

--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -6,67 +6,32 @@ import 'package:notes/view_model/note_list_view_model.dart';
 import 'package:notes/widgets/fab.dart';
 import 'package:notes/widgets/notes_grid.dart';
 import 'package:notes/widgets/notes_list.dart';
+import 'package:notes/widgets/searchBar.dart';
 import 'package:page_transition/page_transition.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Notes'),
-        centerTitle: true,
-        actions: [
-          Consumer(
-            builder: (context, watch, child) {
-              final viewModel = watch(NoteListViewModelProvider);
-              // layout variable is used to determine currently slected layout
-              LayoutType layout = viewModel.layout;
-
-              final selectednotes = watch(SelectedNotesProvider);
-              // [isSelected] will be true if any notes are selected
-              bool isSelected = selectednotes.notes_list.isNotEmpty;
-              // if any notes are selected show delete icon
-              if (isSelected) {
-                return IconButton(
-                    icon: const Icon(Icons.delete),
-                    onPressed: () {
-                      viewModel.deleteMultipleNotes(
-                          selectednotes.notes_list.map((e) => e.id).toList());
-                      selectednotes.clear();
-                    });
-              } else {
-                // if no notes are selected and the current layout is Grid then Show the List Icon.
-                if (layout == LayoutType.Grid) {
-                  return IconButton(
-                    icon: const Icon(Icons.list),
-                    onPressed: () {
-                      viewModel.toggleView();
-                    },
-                  );
-                } else {
-                  // if current layout is List then Show the Grid Icon
-                  return IconButton(
-                    icon: const Icon(Icons.grid_view),
-                    onPressed: () {
-                      viewModel.toggleView();
-                    },
-                  );
-                }
-              }
-            },
-          ),
-        ],
-        // Dark Mode color
-        backgroundColor: const Color(0xFF293440),
-      ),
-      // Dark Mode color
       backgroundColor: const Color(0xFF293440),
-      // body: NotesList(),
-      body: Consumer(
-        builder: (context, watch, child) {
-          LayoutType layout = watch(NoteListViewModelProvider).layout;
-          return layout == LayoutType.Grid ? NotesGrid() : NotesList();
-        },
+      body: SafeArea(
+        child: CustomScrollView(
+//          physics: BouncingScrollPhysics(),
+          slivers: [
+            SearchBar(),
+            Consumer(
+              builder: (context, watch, child) {
+                LayoutType layout = watch(NoteListViewModelProvider).layout;
+                return layout == LayoutType.Grid ? NotesGrid() : NotesList();
+              },
+            ),
+            SliverToBoxAdapter(
+              child: SizedBox(
+                height: 70.0,
+              ),
+            ),
+          ],
+        ),
       ),
       floatingActionButton: Fab(
         onPressed: () {
@@ -78,14 +43,15 @@ class HomePage extends StatelessWidget {
                 // duration: Duration(milliseconds: 200),
                 // alignment: Alignment.bottomRight,
                 // curve: Curves.easeInOutCubic),
-            
-            //   type: PageTransitionType.rightToLeft,
-            //   child: AddNotePage(),
-            //   duration: Duration(milliseconds: 200),curve: Curves.easeInOutCubic
-            
-            type: PageTransitionType.rightToLeft,
+
+                //   type: PageTransitionType.rightToLeft,
+                //   child: AddNotePage(),
+                //   duration: Duration(milliseconds: 200),curve: Curves.easeInOutCubic
+
+                type: PageTransitionType.rightToLeft,
                 child: AddNotePage(),
-                duration: Duration(milliseconds: 200),reverseDuration: Duration(milliseconds: 200),
+                duration: Duration(milliseconds: 200),
+                reverseDuration: Duration(milliseconds: 200),
                 // alignment: Alignment.bottomRight,
                 curve: Curves.easeInOutCubic),
           );
